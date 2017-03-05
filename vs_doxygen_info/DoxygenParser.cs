313995c05ec28b3c58464a0a3c6e451ac12487d9
speedyleion <speedyleion@users.noreply.github.com>
2017-03-04 22:25:55 -0800

    Start adding logic to parse comments

--- vs_doxygen_info/DoxygenParser.cs
+++ vs_doxygen_info/DoxygenParser.cs
@@ -11,5 +11,26 @@ namespace vs_doxygen_info
     /// </summary>
     class DoxygenParser
     {
+        private Dictionary<string, IList<string>> m_dictionary;
+
+        /// <summary>
+        /// These are the known commands we'll handle
+        /// </summary>
+        const private string commands = "[@|\\](brief|param|returns)";
+        public DoxygenParser(string raw_string)
+        {
+            List<string> local_list;
+            local_list.Add(raw_string);
+            m_dictionary.Add("raw", local_list);
+        }
+
+        /// <summary>
+        /// This will parse the string into the specific doxygen blocks
+        /// </summary>
+        /// <returns>The dictionary of all of the string contents parsed</returns>
+        public Dictionary<string, IList<string>> Parse()
+        {
+            return m_dictionary;
+        }
     }
 }
