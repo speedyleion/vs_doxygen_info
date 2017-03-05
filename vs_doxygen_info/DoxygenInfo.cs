bbe2127a27c3245a829464e427f10aa45ca6edd6
speedyleion <speedyleion@users.noreply.github.com>
2017-03-04 23:17:14 -0800

    have the initial regex compiling and running

--- vs_doxygen_info/DoxygenInfo.cs
+++ vs_doxygen_info/DoxygenInfo.cs
@@ -9,107 +9,120 @@ using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
 
-internal class DoxygenInfo : IQuickInfoSource
-
+namespace vs_doxygen_info
 {
-    private TestQuickInfoSourceProvider m_provider;
-    private ITextBuffer m_subjectBuffer;
-    private Dictionary<string, string> m_dictionary;
-    public DoxygenInfo(TestQuickInfoSourceProvider provider, ITextBuffer subjectBuffer)
+    internal class DoxygenInfo : IQuickInfoSource
+
     {
-        m_provider = provider;
-        m_subjectBuffer = subjectBuffer;
+        private TestQuickInfoSourceProvider m_provider;
+        private ITextBuffer m_subjectBuffer;
+        private Dictionary<string, string> m_dictionary;
+        public DoxygenInfo(TestQuickInfoSourceProvider provider, ITextBuffer subjectBuffer)
+        {
+            m_provider = provider;
+            m_subjectBuffer = subjectBuffer;
 
-        //these are the method names and their descriptions
-        m_dictionary = new Dictionary<string, string>();
-        m_dictionary.Add("add", "int add(int firstInt, int secondInt)\nAdds one integer to another.");
-        m_dictionary.Add("subtract", "int subtract(int firstInt, int secondInt)\nSubtracts one integer from another.");
-        m_dictionary.Add("multiply", "int multiply(int firstInt, int secondInt)\nMultiplies one integer by another.");
-        m_dictionary.Add("divide", "int divide(int firstInt, int secondInt)\nDivides one integer by another.");
-    }
-    public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> qiContent, out ITrackingSpan applicableToSpan)
-    {
-        // Map the trigger point down to our buffer.
-        SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint(m_subjectBuffer.CurrentSnapshot);
-        if (!subjectTriggerPoint.HasValue)
-        {
-            applicableToSpan = null;
-            return;
+            //these are the method names and their descriptions
+            m_dictionary = new Dictionary<string, string>();
+            m_dictionary.Add("add", "int add(int firstInt, int secondInt)\nAdds one integer to another.");
+            m_dictionary.Add("subtract", "int subtract(int firstInt, int secondInt)\nSubtracts one integer from another.");
+            m_dictionary.Add("multiply", "int multiply(int firstInt, int secondInt)\nMultiplies one integer by another.");
+            m_dictionary.Add("divide", "int divide(int firstInt, int secondInt)\nDivides one integer by another.");
         }
-
-        ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
-        SnapshotSpan querySpan = new SnapshotSpan(subjectTriggerPoint.Value, 0);
-
-        //look for occurrences of our QuickInfo words in the span
-        ITextStructureNavigator navigator = m_provider.NavigatorService.GetTextStructureNavigator(m_subjectBuffer);
-        TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
-        string searchText = extent.Span.GetText();
-        string lastText = null;
-        foreach (ITextBuffer s in qiContent)
+        public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> qiContent, out ITrackingSpan applicableToSpan)
         {
-            ITextSnapshot x = s.CurrentSnapshot;
-            lastText = x.GetText();
-        }
-        if (lastText != null)
-        {
-            qiContent.Clear();
-            qiContent.Add(this.format(lastText));
-        }
-        foreach (string key in m_dictionary.Keys)
-        {
-            int foundIndex = searchText.IndexOf(key, StringComparison.CurrentCultureIgnoreCase);
-            if (foundIndex > -1)
+            // Map the trigger point down to our buffer.
+            SnapshotPoint? subjectTriggerPoint = session.GetTriggerPoint(m_subjectBuffer.CurrentSnapshot);
+            if (!subjectTriggerPoint.HasValue)
             {
-                applicableToSpan = currentSnapshot.CreateTrackingSpan
-                    (
-                                            //querySpan.Start.Add(foundIndex).Position, 9, SpanTrackingMode.EdgeInclusive
-                                            extent.Span.Start + foundIndex, key.Length, SpanTrackingMode.EdgeInclusive
-                    );
-
-                string value;
-                m_dictionary.TryGetValue(key, out value);
-                if (value != null)
-                    qiContent.Add(value);
-                else
-                    qiContent.Add("");
-
+                applicableToSpan = null;
                 return;
+            }
+
+            ITextSnapshot currentSnapshot = subjectTriggerPoint.Value.Snapshot;
+            SnapshotSpan querySpan = new SnapshotSpan(subjectTriggerPoint.Value, 0);
+
+            //look for occurrences of our QuickInfo words in the span
+            ITextStructureNavigator navigator = m_provider.NavigatorService.GetTextStructureNavigator(m_subjectBuffer);
+            TextExtent extent = navigator.GetExtentOfWord(subjectTriggerPoint.Value);
+            string searchText = extent.Span.GetText();
+            string lastText = null;
+            foreach (ITextBuffer s in qiContent)
+            {
+                ITextSnapshot x = s.CurrentSnapshot;
+                lastText = x.GetText();
+            }
+            if (lastText != null)
+            {
+                qiContent.Clear();
+                qiContent.Add(this.format(lastText));
+            }
+            foreach (string key in m_dictionary.Keys)
+            {
+                int foundIndex = searchText.IndexOf(key, StringComparison.CurrentCultureIgnoreCase);
+                if (foundIndex > -1)
+                {
+                    applicableToSpan = currentSnapshot.CreateTrackingSpan
+                        (
+                                                //querySpan.Start.Add(foundIndex).Position, 9, SpanTrackingMode.EdgeInclusive
+                                                extent.Span.Start + foundIndex, key.Length, SpanTrackingMode.EdgeInclusive
+                        );
+
+                    string value;
+                    m_dictionary.TryGetValue(key, out value);
+                    if (value != null)
+                        qiContent.Add(value);
+                    else
+                        qiContent.Add("");
+
+                    return;
+                }
+            }
+
+            applicableToSpan = null;
+        }
+
+        private string format(string original_content)
+        {
+            DoxygenParser p = new DoxygenParser(original_content);
+            Dictionary<string, IList<string>> dict = p.Parse();
+
+            IList<string> commands;
+            dict.TryGetValue("brief", out commands);
+            if (commands != null)
+            {
+                return commands[0];
+            }
+
+            return original_content;
+        }
+
+        private bool m_isDisposed;
+        public void Dispose()
+        {
+            if (!m_isDisposed)
+            {
+                GC.SuppressFinalize(this);
+                m_isDisposed = true;
             }
         }
 
-        applicableToSpan = null;
     }
 
-    private string format(string original_content)
+    [Export(typeof(IQuickInfoSourceProvider))]
+    [Name("ToolTip QuickInfo Source")]
+    [Order(After = "Default Quick Info Presenter")]
+    [ContentType("text")]
+    internal class TestQuickInfoSourceProvider : IQuickInfoSourceProvider
     {
-        return original_content + "\n\nacos!!!!!!!!!";
-    }
+        [Import]
+        internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
 
-    private bool m_isDisposed;
-    public void Dispose()
-    {
-        if (!m_isDisposed)
+        [Import]
+        internal ITextBufferFactoryService TextBufferFactoryService { get; set; }
+        public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
         {
-            GC.SuppressFinalize(this);
-            m_isDisposed = true;
+            return new DoxygenInfo(this, textBuffer);
         }
-    }
-
-}
-
-[Export(typeof(IQuickInfoSourceProvider))]
-[Name("ToolTip QuickInfo Source")]
-[Order(After = "Default Quick Info Presenter")]
-[ContentType("text")]
-internal class TestQuickInfoSourceProvider : IQuickInfoSourceProvider
-{
-    [Import]
-    internal ITextStructureNavigatorSelectorService NavigatorService { get; set; }
-
-    [Import]
-    internal ITextBufferFactoryService TextBufferFactoryService { get; set; }
-    public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
-    {
-        return new DoxygenInfo(this, textBuffer);
     }
 }
--- vs_doxygen_info/DoxygenParser.cs
+++ vs_doxygen_info/DoxygenParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,11 +17,14 @@ namespace vs_doxygen_info
         /// <summary>
         /// These are the known commands we'll handle
         /// </summary>
-        const private string commands = "[@|\\](brief|param|returns)";
+        private const string commands = "[@|\\\\](brief|param|returns)";
+
         public DoxygenParser(string raw_string)
         {
-            List<string> local_list;
+            List<string> local_list = new List<string>();
             local_list.Add(raw_string);
+
+            m_dictionary = new Dictionary<string, IList<string>>();
             m_dictionary.Add("raw", local_list);
         }
 
@@ -28,8 +32,23 @@ namespace vs_doxygen_info
         /// This will parse the string into the specific doxygen blocks
         /// </summary>
         /// <returns>The dictionary of all of the string contents parsed</returns>
+        /// 
+        /// From http://regexstorm.net/tester?p=%5b%5c%5c%40%5d%28brief%7cparam%7creturns%29%28.*%3f%29%28%3f%3d%28%5b%5c%5c%40%5d%28brief%7cparam%7creturns%29%29%7c%28%5cn%5cn%29%7c%24%29&i=%5cbrief+hello+this+is+my+function%0d%0athis+is+more+about+my+function%0d%0aand+some+more+text%0d%0a%0d%0a%0d%0a%0d%0a%5cparam+first+my+first+param%0d%0a%5cparam+second+this+is+the+second+param&o=s
+        /// wow that is a crazy url.  
+        /// 
+        /// Here is the magic regex.
+        /// [\\@](brief|param|returns)(.*?)(?=([\\@](brief|param|returns))|(\n\n)|$)
+        /// First match a doxygen command such as "\brief" 
+        /// then .*? is an ungreedy match of all characters, note this needs to be done in single line mode to grab newlines.
+        /// Then do a look ahead to see if we found another doxygen command, a blank line, or the end of the string.
         public Dictionary<string, IList<string>> Parse()
         {
+            string pattern = commands + "(.*?)(?=" + commands + "|(\\n\\n)|$)";
+            Regex command_regex = new Regex(pattern, RegexOptions.Singleline);
+
+            IList<string> raw;
+            m_dictionary.TryGetValue("raw", out raw);
+            Match m = command_regex.Match(raw[0]);
             return m_dictionary;
         }
     }
